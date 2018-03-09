927f8105f43fac13be8ca0942a1d19366f8253c2
Jennifer Bach <jmbach1@gmail.com>
2018-03-09 15:24:35 -0800

    answered Q2

--- app.js
+++ app.js
@@ -31,8 +31,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
-}
+    var productOfTwoNumbers = a + b
+    var productString = 'The product of ' + a + ' and ' + b + ' is ' + productOfTwoNumbers + '.';
+    return [productOfTwoNumber, productString]
+  }
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
